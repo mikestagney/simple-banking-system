f29e3e07dce6d3d6f96d65891d31848aec32bf61
mikestagney <mikestagney@yahoo.com>
2021-05-13 16:23:01 -0400

    added menu choices

--- Main.java
+++ Main.java
@@ -106,7 +106,10 @@ public class Main {
     }
     public static void subMenu() {
         System.out.println("1. Balance");
-        System.out.println("2. Log out");
+        System.out.println("2. Add income");
+        System.out.println("3. Do transfer");
+        System.out.println("4. Close account");
+        System.out.println("5. Log out");
         System.out.println("0. Exit");
     }
     public static String getInputFromUser() {
